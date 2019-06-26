3ec2b1daf130d8e181fc96b88b9ba802af13e60a
lancer <123>
2019-06-26 12:01:13 +0800

    从新布局ui

--- script_tools/ws_logger.py
+++ script_tools/ws_logger.py
@@ -1,67 +1,72 @@
 import  os,sys,time,datetime,socket,threading
-from PyQt5.QtCore import *
-from PyQt5.QtGui    import *
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import *
 
 __version__ = "V0.0.1"
 __auther__ = "Lancer"
 
-class  UI(QTabWidget):
+#QTabWidget
+
+class wslogger(QWidget):
     def  __init__(self, parent=None):
-        super(UI,self).__init__(parent)
-        self.initUI()
+        super(wslogger,self).__init__(parent)
+        self.resize(1000, 800)
+        self.setWindowTitle("wslogger 版本号: "+ __version__ + "  作者: " + __auther__ )
+
+        self.tabWidget =  QTabWidget()
+        self.btnWidget =  QTabWidget()
+        self.main_layout =  QVBoxLayout()
+
+        self.textEdit01 =  QTextEdit()
+        self.textEdit02 =  QTextEdit()
+        self.textEdit03 =  QTextEdit()
+        self.textEdit04 =  QTextEdit()
+
+        self.tabWidget.addTab(self.textEdit01  , "APP-CPU1")
+        self.tabWidget.addTab(self.textEdit02  , "APP-CPU2")
+        self.tabWidget.addTab(self.textEdit03  , "SC-CPU1")
+        self.tabWidget.addTab(self.textEdit04  , "SC-CPU2")
         
-    def  initUI(self):
-        #add  four widgets
-        self.sc_cpu1  = QWidget()
-        self.sc_cpu2  = QWidget()
-        self.app_cpu1 = QWidget()
-        self.app_cpu2 = QWidget()
-        # add  top leve
-        self.addTab(self.sc_cpu1  , "sc-cpu1")
-        self.addTab(self.sc_cpu2  , "sc-cpu2")
-        self.addTab(self.app_cpu1 , "app-cpu1 ")
-        self.addTab(self.app_cpu2 , "app-cpu2 ")
+        
+        self.main_layout.addWidget(self.tabWidget)
+        self.main_layout.addWidget(self.btnWidget)
+        self.main_layout.setStretch(0,30)
+        self.main_layout.setStretch(1,1)
+        self.setLayout(self.main_layout)
 
-        self.resize(1000,800)
-        self.setWindowTitle(__version__+"   " +__auther__)
+        self.btnlayout  =   QHBoxLayout(self.btnWidget)
+        self.clear_label =  QLabel("一键清除",self.btnWidget)
+        self.clear_btn   =  QPushButton("清除",self.btnWidget)
+        self.stop_label =   QLabel("底行追踪",self.btnWidget)
+        self.stop_btn   =   QPushButton("Stop",self.btnWidget)
+        self.font_label =   QLabel("字体大小",self.btnWidget)
+        self.fontbox    =   QComboBox(self.btnWidget)
+        self.fontbox.addItem("14")
+        self.fontbox.addItem("15")
+        self.port_label =   QLabel("监听端口",self.btnWidget)
+        self.portbox    =   QComboBox(self.btnWidget)
+        self.portbox.addItem("10100")
+        self.portbox.addItem("10200")
+        self.portbox.addItem("10300")
 
-        #layout
-        self.mainlayout01  =  QVBoxLayout()
-        self.mainlayout02  =  QVBoxLayout()
-        self.mainlayout03  =  QVBoxLayout()
-        self.mainlayout04  =  QVBoxLayout()
+        self.btnlayout.addWidget(self.clear_label)
+        self.btnlayout.addWidget(self.clear_btn)
+        self.btnlayout.addWidget(self.stop_label)
+        self.btnlayout.addWidget(self.stop_btn)
+        self.btnlayout.addWidget(self.font_label)
+        self.btnlayout.addWidget(self.fontbox)
+        self.btnlayout.addWidget(self.port_label)
+        self.btnlayout.addWidget(self.portbox)
+        self.btnlayout.setContentsMargins(0, 0, 0, 0)
+        self.btnlayout.setSpacing(5)
+        self.btnWidget.setLayout(self.btnlayout)
+        #self.btnWidget.setStyleSheet("background:rgba(0,0,0,0.2)")
+        self.setStyleSheet(".QLabel{color:red;} ")
 
-        self.btnlayout01  =   QHBoxLayout()
-        self.btnlayout02  =   QHBoxLayout()
-        self.btnlayout03  =   QHBoxLayout()
-        self.btnlayout04  =   QHBoxLayout()
-
-        self.btn_clear01 =  QPushButton("clear")
-        self.btn_clear02 =  QPushButton()
-        self.btn_clear03 =  QPushButton()
-        self.btn_clear04 =  QPushButton()
-
-        self.lebel_01 = QLabel("一键清除")
-        self.lebel_02 = QLabel()
-        self.lebel_03 = QLabel()
-        self.lebel_04 = QLabel()
-
-        self.btn_frame_sc_cpu1  = QFrame()
-        self.btn_frame_sc_cpu2  = QFrame()
-        self.btn_frame_app_cpu1 = QFrame()
-        self.btn_frame_app_cpu2 = QFrame()
-
-        #wiget01
-        self.btnlayout01.addWidget( self.lebel_01)
-        self.btn_frame_sc_cpu1.setLayout( self.btnlayout01)
-        self.mainlayout01.addWidget(self.btn_frame_sc_cpu1)
-        self.mainlayout01.addWidget(self.btn_clear01)
-        self.sc_cpu1.setLayout( self.mainlayout01)
 
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
-	demo = UI() 	
+	demo = wslogger() 
 	demo.show()
 	sys.exit(app.exec_())
